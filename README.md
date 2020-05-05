5824fd0e3a68d070f5a2129c823326783bab38f7
masashi.g <masashi.g@gmail.com>
2020-05-05 12:08:55 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# WebRTC Insertable Streams DEMO
+
+This Repo is trial of WebRTC Insertable Streams.
+
+Refering:
+- https://github.com/alvestrand/webrtc-media-streams/blob/master/explainer.md
+- WebRTC/Samples - [endtoend-encryption](https://github.com/webrtc/samples/tree/gh-pages/src/content/peerconnection/endtoend-encryption) (BSD 3-Clause LICENSE)
+
+これは WebRTC Insertable Streams のお試しデモです。
+
+参考にしたもの:
+- 説明 ... https://github.com/alvestrand/webrtc-media-streams/blob/master/explainer.md
+- サンプル ... WebRTC/Samples - [endtoend-encryption](https://github.com/webrtc/samples/tree/gh-pages/src/content/peerconnection/endtoend-encryption) (BSD 3-Clause LICENSE)
+
+## LICENSE / ライセンス
+
+- DEMO ... MIT LICENSE / MITライセンス
+- [videopipes.js](https://github.com/webrtc/samples/blob/gh-pages/src/content/peerconnection/endtoend-encryption/js/videopipe.js) (from [webrtc/samples](https://github.com/webrtc/samples)) ... BSD 3-Clause LICENSE
+
+
+# 利用方法
+
+## 事前準備
+
+- Chrome m83 以上で、#enable-experimental-web-platform-features フラグをオン(enabled)に設定
+
+## GitHub Pages で実行
+
+- Chrome m83以上で、ページにアクセス
+- [Start Video]ボタンをクリックし、カメラから映像を取得
+  - 左に映像が表示される
+  - [use Audio]がチェックされていると、マイクの音声も取得
+- [Connect]ボタンをクリック
+  - ブラウザの単一タブ内で2つの「PeerConnectionの通信が確率
+  - 右に受信した映像が表示される
+- ストリームデータの加工
+  - 左の[XOR Sender data]をチェックすると、送信側でストリームのデータを加工
+  - 右の[XOR Receiver data]をチェックすると、受信側でストリームのデータを逆加工
+  - どちらも加工しない、あるいは加工する場合のみ、正常に右の映像が表示できる
+
+- [Disconnect]ボタンをクリックすると通信切断
+- [Stop Video]ボタンをクリックすると、映像取得終了
+
+
+
+
---
+++ videopipe.js
@@ -0,0 +1,69 @@
+/*
+ *  Copyright (c) 2020 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree.
+ */
+//
+// A "videopipe" abstraction on top of WebRTC.
+//
+// The usage of this abstraction:
+// var pipe = new VideoPipe(mediastream, handlerFunction);
+// handlerFunction = function(mediastream) {
+//   do_something
+// }
+// pipe.close();
+//
+// The VideoPipe will set up 2 PeerConnections, connect them to each
+// other, and call HandlerFunction when the stream is available in the
+// second PeerConnection.
+//
+'use strict';
+
+function VideoPipe(stream, forceSend, forceReceive, handler) {
+  this.pc1 = new RTCPeerConnection({
+    forceEncodedVideoInsertableStreams: forceSend,
+    forceEncodedAudioInsertableStreams: forceSend,
+  });
+  this.pc2 = new RTCPeerConnection({
+    forceEncodedVideoInsertableStreams: forceReceive,
+    forceEncodedAudioInsertableStreams: forceReceive,
+  });
+
+  stream.getTracks().forEach((track) => this.pc1.addTrack(track, stream));
+  this.pc2.ontrack = handler;
+}
+
+VideoPipe.prototype.negotiate = async function () {
+  this.pc1.onicecandidate = e => {
+    if (e.candidate) {
+      this.pc2.addIceCandidate(e.candidate);
+    }
+    else {
+      console.log('pc1 candidate empty');
+    }
+  }
+  this.pc2.onicecandidate = e => {
+    if (e.candidate) {
+      this.pc1.addIceCandidate(e.candidate);
+    }
+    else {
+      console.log('pc2 candidate empty');
+    }
+  }
+
+  const offer = await this.pc1.createOffer();
+  //await this.pc2.setRemoteDescription({ type: 'offer', sdp: offer.sdp.replace('red/90000', 'green/90000') });
+  await this.pc2.setRemoteDescription({ type: 'offer', sdp: offer.sdp });
+  await this.pc1.setLocalDescription(offer);
+
+  const answer = await this.pc2.createAnswer();
+  await this.pc1.setRemoteDescription(answer);
+  await this.pc2.setLocalDescription(answer);
+};
+
+VideoPipe.prototype.close = function () {
+  this.pc1.close();
+  this.pc2.close();
+};
